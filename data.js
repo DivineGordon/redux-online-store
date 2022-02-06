77787b8de6523887a96a451f12f9762d136e0bdf
DivineGordon <87007940+DivineGordon@users.noreply.github.com>
2022-02-06 19:54:33 +0000

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,7 @@
 export const inventoryData = [
-  
+   {id:8, name: 'Motar and pestle', price: 40.00, img:"./images/girl_and_motar.jpg"},
+  {id:7, name: 'Hair product', price: 30.00, img:"./images/hair_products.jpg"},
+  {id:6, name: 'Skin care', price: 25.00, img:"./images/skin_care.jpg"},
     {id:0, name: 'Hello World Hat', price: 23.99, img: 'https://static-assets.codecademy.com/Courses/Learn-Redux/codecademy-store/hello-world-hat.jpg' },
     { id:1,des:'Learn From Home Joggers',name: "Joggers", price: 45.99, img: 'https://static-assets.codecademy.com/Courses/Learn-Redux/codecademy-store/sweatpants.jpg' },
     { id:2, name: 'Java Tee', price: 17.99, img: 'https://static-assets.codecademy.com/Courses/Learn-Redux/codecademy-store/java-tee.jpg' },
